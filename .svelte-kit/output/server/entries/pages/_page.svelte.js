a27dbd10f638333a60b64bffbbe8f3f55a649606
suryawan <rizalsuryawan86@gmail.com>
2023-07-13 23:56:28 +0700

    Updates

--- .svelte-kit/output/server/entries/pages/_page.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component, e as escape } from "../../chunks/ssr.js";
-import { b as base } from "../../chunks/paths.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<nav><a href="${escape(base, true) + "/"}">Home</a> <a href="${escape(base, true) + "/about"}">About</a></nav> <h1 data-svelte-h="svelte-130tms">Welcome to My fucking blog</h1> <p data-svelte-h="svelte-jl9sbz">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>`;
-});
-export {
-  Page as default
-};
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.9ea0fe10.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js","_app/immutable/chunks/singletons.a0dfba94.js","_app/immutable/chunks/paths.407244a3.js"];
-export const stylesheets = [];
-export const fonts = [];
